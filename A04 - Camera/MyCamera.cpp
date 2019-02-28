aaaa9e061cd93f1613d68b90e1171e603e4fe5b9
dso2890 <dso2890@IGMOH1370-08>
2019-02-28 14:33:06 -0500

    hw info

--- A04 - Camera/MyCamera.cpp
+++ A04 - Camera/MyCamera.cpp
@@ -153,10 +153,14 @@ void Simplex::MyCamera::CalculateProjectionMatrix(void)
 void MyCamera::MoveForward(float a_fDistance)
 {
 	//The following is just an example and does not take in account the forward vector (AKA view vector)
-	m_v3Position += vector3(0.0f, 0.0f,-a_fDistance);
-	m_v3Target += vector3(0.0f, 0.0f, -a_fDistance);
-	m_v3Above += vector3(0.0f, 0.0f, -a_fDistance);
+	m_v3Position += m_v3Forward * a_fDistance;
+	m_v3Target += m_v3Forward * -a_fDistance;
+	m_v3Above += m_v3Forward * -a_fDistance;
 }
 
+// Make vectors for upward and right then do the same thing as move forward
+// Look in appclass controls / camera location for rotating (glm rotate (quaternion, vector))
 void MyCamera::MoveVertical(float a_fDistance){}//Needs to be defined
+
+
 void MyCamera::MoveSideways(float a_fDistance){}//Needs to be defined
